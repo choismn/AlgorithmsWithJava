77f8c0bfc08bde771a0491b03fa6e93b1f8ac4fa
Sungmin Choi <89376062+choismn@users.noreply.github.com>
2025-08-30 01:02:57 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 145.02 ms, Memory: 63 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        for(String l : line){
+            System.out.print(l);
+        }
+    
+    }
+}
