44d9a0f12ccea887775da4ae6ff7d21f9e79589d
Sungmin Choi <89376062+choismn@users.noreply.github.com>
2025-08-30 16:54:16 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        int[] answer = Arrays.copyOf(num_list, len+1);
+        answer[len] = (num_list[len-2] < num_list[len-1]) ? num_list[len-1] - num_list[len-2] : num_list[len-1]*2;
+        return answer;
+    }
+}
