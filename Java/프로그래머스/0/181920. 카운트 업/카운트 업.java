5d2b2b0d7118e1d9c3d4aa813434c64d928ccaff
Sungmin Choi <89376062+choismn@users.noreply.github.com>
2025-09-04 00:05:27 +0900

    [level 0] Title: 카운트 업, Time: 2.85 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        List<Integer> arr = new ArrayList<>();
+        
+        for(int i=start_num ; i<=end_num ; i++){
+            arr.add(i);
+        }
+        
+        int[] answer = arr.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
