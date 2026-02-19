808594a23323a9896051727ed88ddfd59626ce86
Sungmin Choi <89376062+choismn@users.noreply.github.com>
2026-02-19 20:39:25 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,23 +1,16 @@
 import sys
 
-wholeList = [] 
-
-# line by line input, row = 9
-for i in range(9):
-    line = list(map(int, sys.stdin.readline().split())) 
-    wholeList.append(line)
-
 maxVal = -1
 maxPosX = 0
 maxPosY = 0
 
+# line by line input, row = 9
 for i in range(9):
-    for j in range(9):
-        if wholeList[i][j] > maxVal:
-            maxVal = wholeList[i][j]
-            maxPosX = i+1
-            maxPosY = j+1
+    line = list(map(int, sys.stdin.readline().split())) 
+    if max(line) > maxVal:
+        maxVal = max(line)
+        maxPosY = i+1
+        maxPosX = line.index(maxVal)+1
 
 print(maxVal)
-print(maxPosX, maxPosY)
-
+print(maxPosY, maxPosX)
